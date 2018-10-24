22203c639c54fe7157f9b718c947b29124abca40
PaulaAraya <paula.arayawilliams@gmail.com>
2018-10-24 20:06:27 -0300

    data medicos

---
+++ public/assets/js/data.js
@@ -0,0 +1,102 @@
+const medJson = [
+  {
+    "nombre": " Catalina Larrain Soler",
+    "rut": "61606402-9",
+    "especialidad": "ginecologia",
+    "foto": "https://photos.doctoralia.com/636288346020388786_1.jpg",
+    "comuna": "Providencia",
+    "region": "Metropolitana"
+  },
+  {
+    "nombre": "Ricardo Remenik Schapira",
+    "rut": "13905362-1",
+    "especialidad": "ginecologia",
+    "foto": "https://photos.doctoralia.com/635130717579819135_2.jpg",
+    "comuna": "Providencia",
+    "region": "Metropolitana"
+  },
+  {
+    "nombre": "Francisco Javier Galvez Pavez",
+    "rut": "11869467-8",
+    "especialidad": "ginecologia",
+    "foto": "https://photos.doctoralia.com/636620312737161568_2.jpg",
+    "comuna": "Providencia",
+    "region": "Metropolitana"
+  },
+  {
+    "nombre": "Gustavo Olivares Araya",
+    "rut": "168916671",
+    "especialidad": "Medicina General",
+    "foto": "https://photos.doctoralia.com/636581784749523465_2.jpg",
+    "comuna": "Providencia",
+    "region": "Metropolitana"
+  },
+  {
+    "nombre": "Elizabeth Zuniga",
+    "rut": "16988243-6",
+    "especialidad": "Medicina General",
+    "foto": "https://photos.doctoralia.com/636736696761468841_2.jpg",
+    "comuna": "Providencia",
+    "region": "Metropolitana"
+  },
+  {
+    "nombre": "Dra. Clara Lavin Flores",
+    "rut": "32741-7",
+    "especialidad": "Medicina General",
+    "foto": "https://photos.doctoralia.com/635864972224357365_2.jpg",
+    "comuna": "Providencia",
+    "region": "Metropolitana"
+  },
+  {
+    "nombre": "Jairo Anibal Olascuaga",
+    "rut": "25385356-5",
+    "especialidad": "Oftalmologia",
+    "foto": "https://photos.doctoralia.com/636475887889786372_2.jpg",
+    "comuna": "Providencia",
+    "region": "Metropolitana"
+  },
+  {
+    "nombre": "Esteban Fuentes",
+    "rut": "18910664-5",
+    "especialidad": "Oftalmologia",
+    "foto": "24572586-8",
+    "comuna": "Providencia",
+    "region": "Metropolitana"
+  },
+  {
+    "nombre": "Carmen Luz Gonzalez Ramirez",
+    "rut": "16994171-8",
+    "especialidad": "Oftalmologia",
+    "foto": "https://photos.doctoralia.com/636699872159287438_2.jpg",
+    "comuna": "Provicencia",
+    "region": "Metropolitana"
+  },
+  {
+    "nombre": "Cristián Seiltgens",
+    "rut": "15591365-7",
+    "especialidad": "Pediatria",
+    "foto": "https://photos.doctoralia.com/636514519497877513_2.jpg",
+    "comuna": "Providencia",
+    "region": "Metropolitana"
+  },
+  {
+    "nombre": "Ana Moya Olivares",
+    "rut": "15024365-3",
+    "especialidad": "Pediatria",
+    "foto": "https://photos.doctoralia.com/636713329221768019_2.jpg",
+    "comuna": "Providencia",
+    "region": "Metropolitana"
+  },
+  {
+    "nombre": "Daniela Carrancio",
+    "rut": "17679591-3",
+    "especialidad": "Pediatria",
+    "foto": "https://photos.doctoralia.com/636529932829412032_1.jpg",
+    "comuna": "Providencia",
+    "region": "Metropolitana"
+  }
+]
+
+medJson.forEach(function(element) {
+  nameMed = element.nombre;
+});
