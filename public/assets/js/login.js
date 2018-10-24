08d85f677c398551755f3e6841e3e7fcad9fd17d
nahytar <nahytar@gmail.com>
2018-10-24 19:58:09 -0300

    se corrige login google

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,21 +1,25 @@
-var uiConfig = {
-  signInSuccessUrl: '/',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+document.addEventListener('DOMContentLoaded', function () {
 
-  ],
-  // Terms of service url.
-  tosUrl: '/'
-};
+  try {
+    let app = firebase.app();
+    var uiConfig = {
+      signInSuccessUrl: '/',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
 
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
+      ],
+      // Terms of service url.
+      tosUrl: '/'
+    };
 
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
 
-const singOut = () => {
-  firebase.auth().signOut()
-}
+  } catch (e) {
+    console.error(e);
+  }
+});
